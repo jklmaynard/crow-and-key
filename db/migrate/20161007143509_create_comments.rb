9cf6127fd171a4bcf900bbad861b1f870cacd7ec
James Maynard <jameskirkmaynard@gmail.com>
2016-10-07 07:40:09 -0700

    Initial tables and schema

--- db/migrate/20161007143509_create_comments.rb
+++ db/migrate/20161007143509_create_comments.rb
@@ -1,6 +1,10 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
+      t.integer :user_id
+      t.integer :book_id
+      t.integer :question_id
+      t.string :text
     end
   end
 end
--- db/migrate/20161007143516_create_questions.rb
+++ db/migrate/20161007143516_create_questions.rb
@@ -1,6 +1,9 @@
 class CreateQuestions < ActiveRecord::Migration[5.0]
   def change
     create_table :questions do |t|
+      t.integer :user_id
+      t.integer :book_id
+      t.string :text
     end
   end
 end
