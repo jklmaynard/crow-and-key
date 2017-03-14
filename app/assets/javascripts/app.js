7d1c76ebcac268baf23ce71c432089a373306716
James Maynard <jameskirkmaynard@gmail.com>
2017-03-14 09:35:28 -0700

    add devise functionality in navCtrl, build out Auth folders with controller and partials for login and registering; include new  params in app.js for login/register

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,20 +4,39 @@ angular.module('crowAndKey', ['ui.router', 'templates', 'Devise'])
   '$urlRouterProvider',
   function($stateProvider, $urlRouterProvider) {
     $stateProvider.state('home', {
-        url: '/home',
-        templateUrl: 'home/_home.html',
-        controller: 'HomeCtrl',
-        resolve: {
-          bookPromise: ['books', function(books) {
-            return books.getAll();
-          }]
-        }
+      url: '/home',
+      templateUrl: 'home/_home.html',
+      controller: 'HomeCtrl',
+      resolve: {
+        bookPromise: ['books', function(books) {
+          return books.getAll();
+        }]
       }
-    );
+    });
     $stateProvider.state('books', {
-        url: '/books/{id}',
-        templateUrl: 'books/_books.html',
-        controller: 'BooksCtrl'
+      url: '/books/{id}',
+      templateUrl: 'books/_books.html',
+      controller: 'BooksCtrl'
+    });
+    $stateProvider.state('login', {
+      url: '/login',
+      templateUrl: 'auth/_login.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'Auth', function($state, Auth) {
+        Auth.currentUser().then(function() {
+          $state.go('home');
+        });
+      }]
+    });
+    $stateProvider.state('register', {
+      url: '/register',
+      templateUrl: 'auth/_register.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'Auth', function($state, Auth) {
+        Auth.currentUser().then(function() {
+          $state.go('home');
+        });
+      }]
     });
     $urlRouterProvider.otherwise('home');
   }
